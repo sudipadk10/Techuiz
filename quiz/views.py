2b037539bd2c4f48327c8293410f1f62f48f4e2d
sudipadk10 <adhikarisudip869@gmail.com>
2025-07-22 22:08:27 +0545

    dropdown fixed , theme fixed

--- quiz/views.py
+++ quiz/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 def index(request):
+    
     return render(request,'index.html')
 
 def login_view(request):
